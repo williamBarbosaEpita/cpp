eac592173a1f7c3d9f51a409773478d9ea5380bc
William Barbosa <william.barbosa-souto-amado@epita.fr>
2022-10-08 22:58:10 +0200

    celaning

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -7,6 +7,8 @@ bool isPrime(int n)
         return false;
     if(n == 2)
         return true;
+    if (n%2 == 0)
+	return false;
     for (int i = 3; i< n/2; i+=2)
         if (n % i == 0)
             return false;
