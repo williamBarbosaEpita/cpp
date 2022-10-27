cca83ad74f631ea9b2b200a1b01b32d01bf9375a
William Barbosa <william.barbosa-souto-amado@epita.fr>
2022-10-27 11:48:59 +0200

    fixing isprime

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -1,21 +1,25 @@
 #include<iostream>
 using namespace std;
 
+long long square_root(long long n){
+    long long i = 0;
+    while(i * i < n)
+        i++;
+    return i; 
+}
+
 bool isPrime(unsigned long int n)
 {
-    if (n <= 0 || n == 1)
+    if (n <= 1)
         return false;
-    if(n == 2)
+    if (n <= 3)
         return true;
-    if (n%2 == 0)
-	return false;
-    for (unsigned long int i = 3; i< n/2; i+=2)
-    {
+    if (n % 2 == 0)
+	    return false;
+    long long racine = square_root(n);
+    for (unsigned long int i = 3; i <= racine; i += 1)
         if (n % i == 0)
             return false;
-        if (i * i > n)
-            break;
-    }
     return true;
 }
 
